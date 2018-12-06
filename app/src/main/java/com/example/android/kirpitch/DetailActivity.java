cb2aa20cd910c209be886b5eac6c80b6adf27efe
Toure Nathan <toure21@live.com>
2018-12-06 16:02:51 +0300

    Created a view for details of a task

--- app/src/main/java/com/example/android/kirpitch/DetailActivity.java
+++ app/src/main/java/com/example/android/kirpitch/DetailActivity.java
@@ -2,8 +2,10 @@ package com.example.android.kirpitch;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -11,5 +13,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
